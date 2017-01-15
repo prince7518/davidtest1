6e8c8115a5f44cbd4c7c73cf0de93bc4542e41a6
peter <pantungmin@hotmail.com>
2017-01-15 21:18:16 +0800

    review examples of make

--- make/gcc_procedure/1/main.c
+++ make/gcc_procedure/1/main.c
@@ -1,8 +1,15 @@
 #include "libfoo.h"
 #define HELLO "hello worild"
+#if DEBUG
+#define MSG(fmt, a...)  printf(fmt, ##a)
+#else
+#define MSG(fmt, a...) do {} while(0)
+#endif
+
 int main(int argc, char *argv[])
 {
 	char *say = HELLO;
 	printf("%s\n", say);
+	MSG("this is debug %s\n", HELLO);
 	libfoo();
 }
